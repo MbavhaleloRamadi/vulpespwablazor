5a3fec428768a96f283b1094b8c40e449a640485
Mbavhalelo Innocent Ramadi <mbavharamadi@gmail.com>
2026-02-26 08:27:35 +0200

    Confimation Page Blazor Component

--- obj/Debug/net8.0/VulpesFormsApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/VulpesFormsApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VulpesFormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88fde523df9b918530477570a107109660b2cdc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9a966ced0003fd8302d8f3069e577196c9dcbd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("VulpesFormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VulpesFormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
